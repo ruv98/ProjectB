618a48d62a952a71124f2e6c21f4fb1853ce294b
0Cina0 <80523769+0Cina0@users.noreply.github.com>
2021-03-12 13:30:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Restaurant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("_________________________\n\nOpening Hours\n\nMonday:     CLOSED\nTuesday:    16:00-20:00\n" +
+                "Wednesday:  16:00-20:00\nThursday:   16:00-20:00\nFriday:     16:00-21:00\n" +
+                "Satuday:    16:00-21:00\nSunday:     16:00-21:00\n_________________________\n");
+            Console.WriteLine("Contact Us\n\nPhone Number: 071-5119113\nEmail: food@restaurant.com");
+        }
+    }
+}
