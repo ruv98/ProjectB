8b17f0738fa2eafbc3695515ad86c749c09dd101
ruv98 <75735511+ruv98@users.noreply.github.com>
2021-03-16 11:25:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjectB
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Dish1\nDish2\nDish3\nDish4\nDish5\nDish6\nDish7\nDish8\nDish9\nDish10");
+
+            
+            Console.Write("Enter the dishnumber you want to view: ");
+            int dishNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You want to view dish: " + dishNumber);
+        }
+
+        
+            
+    }
+}
